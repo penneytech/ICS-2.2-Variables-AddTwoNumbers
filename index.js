88bc741d0b79567ae88a515c752fb77a8ec3fc74
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-05 19:49:47 -0400

    First Commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+
+You are building an app that asks a user two numbers, adds them
+together, and prints the result. 
+
+*/
+ 
+//Declare a variable for x.
+
+//Declare a variable for y.
+
+//Declare a variable for sum.
+
+//Prompt the user "X: " and put the answer variable x.
+
+//Prompt the user "Y: " and put the answer variable y.
+
+//Set sum to equal x + y.
+
+//Tell user "Answer is: " sum in the console log.

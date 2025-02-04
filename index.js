64f5c9c7cb03fc6c5d03f83a0bd86f94375e9dc5
penneytech <penneyp@hdsb.ca>
2025-02-04 09:49:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ together, and prints the result.
 
 */
  
-//Declare a variable for x.
+//Declare a variable for x - do not assign it an initidial value.
 
-//Declare a variable for y.
 
-//Declare a variable for sum.
+//Declare a variable for y - do not assign it an initidial value.
+
+
+//Declare a variable for sum - do not assign it an initidial value.
+
 
 //Prompt the user "X: " and put the answer variable x.
 
+
 //Prompt the user "Y: " and put the answer variable y.
+
 
 //Set sum to equal x + y.
 
-//Tell user "Answer is: " sum in the console log.
+
+//Tell user "Answer is: " and console log the sum. 
+

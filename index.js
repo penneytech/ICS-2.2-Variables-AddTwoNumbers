d2111c04531aab3bd2cf68cd79e0e616d43eb90b
penneytech <penneyp@hdsb.ca>
2025-02-18 15:16:25 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -3,25 +3,26 @@
 You are building an app that asks a user two numbers, adds them
 together, and prints the result. 
 
-*/
+*/ 
  
 //Declare a variable for x - do not assign it an initidial value.
-
+let x;
 
 //Declare a variable for y - do not assign it an initidial value.
-
+let y;
 
 //Declare a variable for sum - do not assign it an initidial value.
+let sum; 
 
+//Prompt the user "X: " and put the answer in variable x.
+x = prompt("What is the value of X?");
 
-//Prompt the user "X: " and put the answer variable x.
-
-
-//Prompt the user "Y: " and put the answer variable y.
-
+//Prompt the user "Y: " and put the answer in variable y.
+y = prompt("What is the value of Y?");
 
 //Set sum to equal x + y.
-
+sum = x + y;
 
 //Tell user "Answer is: " and console log the sum. 
+console.log("The answer is: ", sum);
 
